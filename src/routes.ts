d110635d1868c07718d3e5914173a7bbc3b8afe7
anneedesign-ui <anneedesign@gmail.com>
2025-12-17 18:35:40 +0900

    refactor: 라우터 경로 상수화 (routes.ts 추가)

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  HOME: '/',
+  COMMUNITY: '/community',
+  COMMUNITY_DETAIL: '/community/:id',
+  COMMUNITY_CREATE: '/community/create',
+  COMMUNITY_EDIT: '/community/edit/:id',
+} as const;
+
+// 동적 경로 생성 헬퍼 함수
+export const getRoutes = {
+  communityDetail: (id: string | number) => `/community/${id}`,
+  communityEdit: (id: string | number) => `/community/edit/${id}`,
+};
