49a00c66db5c09c16e5d850c66b1d75d5b85b27f
kimsowon <sowon836@gmail.com>
2025-12-17 16:05:16 +0900

    feat: 공통 UI 확장: Textarea, Dialog, Card(#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
 import '@/App.css';
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTrigger,
-} from './components/ui/dialog';
-import { DialogClose } from './components/ui/dialog';
-import { Button } from './components/ui/button';
-import { InputGroupCustom } from './components/ui/inputwithButton';
-// import { ExternalLinkIcon } from 'lucide-react'
-
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemMedia,
-  ItemTitle,
-} from '@/components/ui/item';
-import { ThumbsUp } from 'lucide-react';
-import { DeleteDialog } from './components/ui/deletedialog';
-
 function App() {
   return (
     <main
@@ -39,59 +15,6 @@ function App() {
     >
       <h1>커뮤니티 프로젝트 베이스</h1>
       <p>여기서부터 비회원 / 회원 / 작성자 플로우</p>
-
-      {/* 댓글창 */}
-      <InputGroupCustom />
-
-      {/* 삭제 */}
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button
-            variant="outline"
-            className="px-[12px] py-[10px] text-[12px] outline-none"
-          >
-            삭제
-          </Button>
-        </DialogTrigger>
-        <DeleteDialog />
-      </Dialog>
-
-      {/* 카드 */}
-      <div className="flex h-[216px] w-[944px] flex-col gap-4">
-        <Item
-          className="h-full w-full items-start justify-start"
-          variant="default"
-          asChild
-        >
-          <a href="" target="_blank" rel="noopener noreferrer">
-            <ItemContent>
-              <div className="flex flex-col gap-2">
-                <ItemDescription>예시 카테고리</ItemDescription>
-                <ItemTitle className="text-[18px]">예시 게시글 제목</ItemTitle>
-                <ItemDescription>
-                  예시 게시글 내용 예시 게시글 내용 예시 게시글 내용 예시 게시글
-                  내용 예시 게시글 내용 예시 게시글 내용 예시 게시글 내용 예시
-                  게시글 내용 예시 게시글 내용 예시 게시글 내용 예시 게시글 내용
-                </ItemDescription>
-              </div>
-              <ItemActions className="flex gap-2">
-                <ThumbsUp />
-                <p>
-                  좋아요
-                  <span>10</span>
-                </p>
-              </ItemActions>
-            </ItemContent>
-            <ItemMedia className="h-[168px] w-[228px]">
-              <img
-                src="./src/assets/image.png"
-                alt="예시 게시글 이미지"
-                className="bg-oz-gray-light h-full w-full rounded-lg object-cover"
-              />
-            </ItemMedia>
-          </a>
-        </Item>
-      </div>
     </main>
   );
 }
---
+++ src/components/ui/card.tsx
@@ -0,0 +1,68 @@
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from './item';
+import { ThumbsUp } from 'lucide-react';
+
+export function Card() {
+  return (
+    <div className="flex h-[216px] w-[944px] flex-col gap-4">
+      <Item
+        className="h-full w-full items-start justify-start"
+        variant="default"
+        asChild
+      >
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <ItemContent className="h-full">
+            <div className="flex flex-col gap-2">
+              <ItemDescription>예시 카테고리</ItemDescription>
+              <ItemTitle className="text-[18px]">예시 게시글 제목</ItemTitle>
+              <ItemDescription>
+                예시 게시글 내용 예시 게시글 내용 예시 게시글 내용 예시 게시글
+                내용 예시 게시글 내용 예시 게시글 내용 예시 게시글 내용 예시
+                게시글 내용 예시 게시글 내용 예시 게시글 내용 예시 게시글 내용
+              </ItemDescription>
+            </div>
+            <div className="mt-auto flex w-full items-center justify-between">
+              <ItemActions className="text-oz-gray-white-dark mt-auto flex gap-2">
+                <ThumbsUp className="color-oz-gray-white-dark" />
+                <p className="flex gap-1 text-[12px]">
+                  <span>좋아요</span>
+                  <span>10</span>
+                </p>
+                <p className="flex gap-1 text-[12px]">
+                  <span>댓글</span>
+                  <span>10</span>
+                </p>
+                <p className="flex gap-1 text-[12px]">
+                  <span>조회수</span>
+                  <span>10</span>
+                </p>
+              </ItemActions>
+              <div className="flex items-center gap-2">
+                <img
+                  src="./src/assets/image.png"
+                  alt="예시 게시글 이미지"
+                  className="bg-oz-gray-light h-[24px] w-[24px] rounded-full object-cover"
+                />
+                <span className="text-[12px]">김소원</span>
+                <span className="text-[12px]">1시간 전</span>
+              </div>
+            </div>
+          </ItemContent>
+          <ItemMedia className="h-[168px] w-[228px]">
+            <img
+              src="./src/assets/image.png"
+              alt="예시 게시글 이미지"
+              className="bg-oz-gray-light h-full w-full rounded-lg object-cover"
+            />
+          </ItemMedia>
+        </a>
+      </Item>
+    </div>
+  );
+}
