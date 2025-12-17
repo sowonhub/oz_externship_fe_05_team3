842c04fafe4556cf12dc7acce1d7dbdbcc7083be
kimsowon <sowon836@gmail.com>
2025-12-17 14:05:31 +0900

    feat: deleteDialog

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
-import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from '@/lib/utils'
+import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -41,7 +41,7 @@ const buttonVariants = cva(
       round: 'default',
     },
   }
-)
+);
 
 function Button({
   className,
@@ -52,9 +52,9 @@ function Button({
   ...props
 }: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : 'button'
+  const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
@@ -62,7 +62,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className, round }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
--- src/components/ui/inputwithbutton.tsx
+++ src/components/ui/inputwithbutton.tsx
@@ -1,22 +1,26 @@
-import { InputGroup, InputGroupAddon } from '@/components/ui/input-group'
-import TextareaAutosize from 'react-textarea-autosize'
-import { InputGroupButton } from './input-group'
+import { InputGroup, InputGroupAddon } from '@/components/ui/input-group';
+import TextareaAutosize from 'react-textarea-autosize';
+import { InputGroupButton } from './input-group';
 
 export function InputGroupCustom() {
   return (
-    <div className="grid w-full max-w-sm gap-6">
+    <div className="focus-within:border-oz-purple grid h-[120px] w-full max-w-[868px] gap-6">
       <InputGroup>
         <TextareaAutosize
           data-slot="input-group-control"
-          className="flex field-sizing-content min-h-16 w-full resize-none rounded-md bg-transparent px-3 py-2.5 text-base transition-[color,box-shadow] outline-none md:text-sm"
-          placeholder="Autoresize textarea..."
+          className="flex field-sizing-content min-h-16 w-full resize-none rounded-md bg-transparent px-3 py-2.5 text-base transition-[color,box-shadow] outline-none md:text-[16px]"
+          placeholder="개인정보를 공유 및 요청하거나, 명예 회손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있습니다."
         />
         <InputGroupAddon align="block-end">
-          <InputGroupButton className="ml-auto" size="sm" variant="default">
-            Submit
+          <InputGroupButton
+            className="focus-within:border-oz-purple bg-oz-gray-white text-oz-gray-dark hover:bg-oz-purple-light active:bg-oz-purple ml-auto h-[40px] w-[80px] rounded-full text-[16px] active:text-white"
+            size="sm"
+            variant="outline"
+          >
+            등록
           </InputGroupButton>
         </InputGroupAddon>
       </InputGroup>
     </div>
-  )
+  );
 }
