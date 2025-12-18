8a1469bf8bc061ecd49a3a03430ccb7d14d5fd95
kimsowon <sowon836@gmail.com>
2025-12-18 16:20:35 +0900

    import input: search bar

--- src/pages/community/communitypage.tsx
+++ src/pages/community/communitypage.tsx
@@ -1,17 +1,29 @@
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '@/components/ui/input-group';
 import { Pagination } from '@/components/ui/pagination';
 import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
 import { NavigationMenu } from '@radix-ui/react-navigation-menu';
+import { SearchIcon } from 'lucide-react';
 
 function CommunityPage() {
   return (
     <div className="flex flex-col gap-4">
-      <h1>커뮤니티</h1>
+      <h1 className="text-[32px] font-bold">커뮤니티</h1>
       <div className="flex items-center gap-4">
         <DropdownMenu></DropdownMenu>
-        <Input placeholder="질문검색" />
+        <InputGroup className="h-[48px] w-[472px] rounded-full">
+          <InputGroupInput className="text-[14px]" placeholder="질문검색" />
+          <InputGroupAddon>
+            <SearchIcon />
+          </InputGroupAddon>
+        </InputGroup>
+
         <Button>글쓰기</Button>
       </div>
       <div>
