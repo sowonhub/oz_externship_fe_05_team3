43e816c07aa66c429406410bf96359b6e58a955b
kimsowon <sowon836@gmail.com>
2025-12-18 21:19:11 +0900

    add select

---
+++ src/components/CommunityPage/searchSelect.tsx
@@ -0,0 +1,27 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+export function SearchSelect() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[120px] text-[16px]">
+        <SelectValue placeholder="검색 유형" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup className="">
+          <SelectLabel>검색 유형</SelectLabel>
+          <SelectItem value="title">제목</SelectItem>
+          <SelectItem value="content">내용</SelectItem>
+          <SelectItem value="author">작성자</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
