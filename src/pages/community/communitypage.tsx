cadea4848dd10357723b9ce3fccff5b05910de67
kimsowon <sowon836@gmail.com>
2025-12-18 15:31:00 +0900

    import App.tsx > App.css

--- src/pages/community/communitypage.tsx
+++ src/pages/community/communitypage.tsx
@@ -1,10 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Pagination } from '@/components/ui/pagination';
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
+import { NavigationMenu } from '@radix-ui/react-navigation-menu';
+
 function CommunityPage() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>커뮤니티 목록 페이지</h1>
-      <p>소원님 작업 예정</p>
+    <div className="flex flex-col gap-4">
+      <h1>커뮤니티</h1>
+      <div className="flex items-center gap-4">
+        <DropdownMenu></DropdownMenu>
+        <Input placeholder="질문검색" />
+        <Button>글쓰기</Button>
+      </div>
+      <div>
+        <NavigationMenu></NavigationMenu>
+        <DropdownMenu></DropdownMenu>
+      </div>
+      <hr />
+      <div>
+        <Card />
+      </div>
+      <hr />
+      <Pagination />
     </div>
-  )
+  );
 }
 
-export default CommunityPage
+export default CommunityPage;
