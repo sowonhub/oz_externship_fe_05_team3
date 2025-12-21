5ba325a1f75b16742a3683c94c98468fbfc48b0a
kimsowon <sowon836@gmail.com>
2025-12-21 22:32:48 +0900

    post Data Transfer Object

---
+++ src/api/model/postDTO.ts
@@ -0,0 +1,25 @@
+export interface PostListResponseDTO {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PostDTO[];
+}
+
+export interface PostDTO {
+  id: number;
+  author: UserDTO;
+  title: string;
+  thumbnail_img_url: string;
+  content_preview: string;
+  comment_count: number;
+  view_count: number;
+  like_count: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface UserDTO {
+  id: number;
+  nickname: string;
+  profile_img_url: string;
+}
