3617ac65d0191c1262b492d3230804a8fbd9e937
kimsowon <sowon836@gmail.com>
2025-12-22 10:51:25 +0900

    edit title name

--- src/components/CommunityPage/CreateButton.tsx
+++ src/components/CommunityPage/CreateButton.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Pencil } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
-export function SearchButton() {
+export function CreateButton() {
   return (
     <Link to="/community/create" className="">
       <Button variant="default" className="h-[48px] w-[120px] text-[16px]">
