52996e07cc05d4c2ffee88ceb437f174ddc5990a
kimsowon <sowon836@gmail.com>
2025-12-29 15:24:09 +0900

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +9,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@types-interface': path.resolve(__dirname, './src/types-interface'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      // '@domains': path.resolve(__dirname, './src/domains'),
+      // '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
-})
+});
