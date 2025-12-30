e5fc40fe45760b9977dad95a08b29e967d8f5d1a
kimsowon <sowon836@gmail.com>
2025-12-30 10:14:47 +0900

    delete

--- src/types-interface/Auth.ts
+++
@@ -1,8 +0,0 @@
-import type { Author } from '@types-interface/index';
-
-export default interface Auth {
-  user: Author | null;
-  isAuthenticated: boolean;
-  login: (user: Author) => void;
-  logout: () => void;
-}
