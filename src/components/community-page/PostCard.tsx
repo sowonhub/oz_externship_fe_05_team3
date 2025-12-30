8e7e25805d1a52ad58453932272f8a8ab71d3ef7
kimsowon <sowon836@gmail.com>
2025-12-30 11:05:12 +0900

    edit postcard ui ( item -> card )

--- src/components/community-page/PostCard.tsx
+++ src/components/community-page/PostCard.tsx
@@ -1,14 +1,13 @@
 import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemMedia,
-  ItemTitle,
+  Card,
+  CardDescription,
+  CardAction,
+  CardTitle,
+  CardContent,
 } from '@/lib/index';
 import { ThumbsUp } from 'lucide-react';
 import { formatRelativeDate } from '@/utils/index';
-import type { Post } from '@/types-interface/index';
+import type { Post } from '@/types/index';
 
 interface PostCardProps {
   postcard: Post;
@@ -28,65 +27,56 @@ const PostCard = ({ postcard }: PostCardProps) => {
   } = postcard;
 
   return (
-    <div className="flex h-[216px] w-[944px] flex-col gap-4">
-      <Item
-        className="h-full w-full items-start justify-start"
-        variant="default"
-        asChild
-      >
-        <a href="" target="_blank" rel="noopener noreferrer">
-          <ItemContent className="h-full">
-            <div className="flex flex-col gap-2">
-              <ItemDescription className="text-oz-gray-dark text-[12px]">
-                {category}
-              </ItemDescription>
-              <ItemTitle className="text-[18px]">{title}</ItemTitle>
-              <ItemDescription className="text-oz-gray text-[14px]">
-                {content_preview}
-              </ItemDescription>
-            </div>
-            <div className="mt-auto flex w-full items-center justify-between">
-              <ItemActions className="text-oz-gray-white-dark mt-auto flex gap-2">
-                <ThumbsUp className="text-oz-gray-white-dark" />
-                <p className="flex gap-1 text-[12px]">
-                  <span>좋아요</span>
-                  <span>{like_count}</span>
-                </p>
-                <p className="flex gap-1 text-[12px]">
-                  <span>댓글</span>
-                  <span>{comment_count}</span>
-                </p>
-                <p className="flex gap-1 text-[12px]">
-                  <span>조회수</span>
-                  <span>{view_count}</span>
-                </p>
-              </ItemActions>
-              <div className="flex items-center gap-2">
-                <img
-                  src={author.profile_img_url || '/src/assets/user.png'}
-                  alt="프로필"
-                  className="bg-oz-gray-light h-[24px] w-[24px] rounded-full object-cover"
-                />
-                <span className="text-oz-gray-dark text-[12px]">
-                  {author.nickname}
-                </span>
-                <span className="text-oz-gray text-[12px]">
-                  {formatRelativeDate(created_at)}
-                </span>
+    <div>
+      <div className="flex h-[216px] w-[944px] flex-col items-center justify-center gap-4">
+        <div className="h-full w-full items-center justify-between">
+          <Card className="flex h-full w-full items-center justify-between">
+            <CardContent className="flex h-full w-full items-center justify-between gap-4">
+              <div className="flex h-full w-full flex-col justify-between">
+                <div className="flex flex-col gap-2">
+                  <CardDescription className="text-oz-gray-dark text-[12px]">
+                    {category}
+                  </CardDescription>
+                  <CardTitle className="text-[18px]">{title}</CardTitle>
+                  <CardDescription className="text-oz-gray text-[14px]">
+                    {content_preview}
+                  </CardDescription>
+                </div>
+                <div className="flex w-full items-center justify-between">
+                  <CardAction className="text-oz-gray-white-dark mt-auto flex items-center gap-2">
+                    <ThumbsUp className="text-oz-gray-white-dark" />
+                    <p className="text-[12px]">좋아요&nbsp;{like_count}</p>
+                    <p className="text-[12px]">댓글&nbsp;{comment_count}</p>
+                    <p className="text-[12px]">조회수&nbsp;{view_count}</p>
+                  </CardAction>
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={author.profile_img_url || '/src/assets/user.png'}
+                      alt="프로필"
+                      className="bg-oz-gray-light h-[24px] w-[24px] rounded-full object-cover"
+                    />
+                    <span className="text-oz-gray-dark text-[12px]">
+                      {author.nickname}
+                    </span>
+                    <span className="text-oz-gray text-[12px]">
+                      {formatRelativeDate(created_at)}
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </ItemContent>
-          {thumbnail_img_url && (
-            <ItemMedia className="ml-10 h-[168px] w-[228px]">
-              <img
-                src={thumbnail_img_url}
-                alt="썸네일"
-                className="bg-oz-gray-light h-full w-full rounded-lg object-cover"
-              />
-            </ItemMedia>
-          )}
-        </a>
-      </Item>
+              {thumbnail_img_url && (
+                <div className="flex h-[163px] w-[228px] items-center justify-center">
+                  <img
+                    src={thumbnail_img_url}
+                    alt="썸네일"
+                    className="bg-oz-gray-light h-full w-full rounded-lg"
+                  />
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
